12e68edee99b241e02da507c4dedc397a06cb235
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2021-05-18 21:13:32 +0700

    fix modal & update data.json
    -fix modal close when click it
    +update data.json

--- src/components/ModalPost.js
+++ src/components/ModalPost.js
@@ -16,11 +16,9 @@ let ModalPost = (props) => {
     }
     let imgUrl = "assets/" + data.number.toString() + ".JPG";
     return (
-        <div
-            onClick={() => onBgClick(null)}
-            className="ModalPost z-50 fixed w-screen h-screen  bg-gray-900 bg-opacity-80 flex items-center justify-center"
-        >
-            <div className="main-box bg-white rounded-3xl  lg:flex-row lg:h-4/5 lg:w-2/3 w-4/5 flex flex-col items-center">
+        <div className="ModalPost z-50 fixed w-screen h-screen  bg-gray-900 bg-opacity-80 flex items-center justify-center">
+            <div onClick={() => onBgClick(null)} className="w-screen h-screen fixed"></div>
+            <div className="main-box z-20 bg-white rounded-3xl  lg:flex-row lg:h-4/5 lg:w-2/3 w-4/5 flex flex-col items-center">
                 <img
                     src={imgUrl}
                     className=" rounded-t-2xl lg:rounded-l-2xl lg:rounded-tr-none w-full lg:w-3/5 flex-grow-0 object-right-down h-56 lg:h-full object-cover"
@@ -30,7 +28,7 @@ let ModalPost = (props) => {
                     <div className="name flex flex-col items-center  my-3 lg:justify-center">
                         <span className="firstname text-7xl text-center">{data.name}</span>
                         <span className="second text-5xl text-center">{data.sirname}</span>
-                        <span className="id text-xl">
+                        <span className="id text-xl lg:text-2xl">
                             เลขที่ {data.number} | รหัสประจำตัวนักเรียน {data.studentID}
                         </span>
 
