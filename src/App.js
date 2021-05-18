a4619f1b490215b971e3b73e04140786629e4594
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2021-05-18 12:19:35 +0700

    change data structure + new font
    +add keyword array in all object
    +change to data.json instead of data.js
    +add HelvethaicaMon font

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import AppHeader from "./components/AppHeader";
 import Card from "./components/Card";
-import datas from "./data/data.js";
+import datas from "./data/data.json";
 import Find from "./components/Find";
 
 function App() {
     const [find, setFind] = useState("");
-
     const filteredDatas = datas.filter((data) => {
         let number = data.number.toString();
         let numberID = data.studentID.toString();
+        let name = data.name.toLowerCase();
+        let sirname = data.sirname.toLowerCase();
+        let university = data.university.toLowerCase();
+        let faculty = data.faculty.toLowerCase();
+        let keywords = data.keywords?.map((keyword) => keyword.toLowerCase());
         return (
             number.includes(find) ||
-            data.name.includes(find) ||
-            data.sirname.includes(find) ||
-            data.university.includes(find) ||
-            data.faculty.includes(find) ||
-            numberID.includes(find)
+            numberID.includes(find) | name.includes(find) ||
+            sirname.includes(find) ||
+            university.includes(find) ||
+            faculty.includes(find) ||
+            keywords?.some((keyword) => keyword.includes(find.toLowerCase()))
         );
     });
 
