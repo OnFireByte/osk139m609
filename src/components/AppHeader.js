4d5bfd81a108bf8aecad57db44bd7239923f75c9
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2021-05-20 02:19:53 +0700

    make navbar frosted

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,10 +1,11 @@
 import Find from "./Find";
+import "./AppHeader.css";
 
 let AppHeader = (props) => {
     const { findValue, onFindValueChange } = props;
 
     return (
-        <div className="AppHeader z-10 shadow-md fixed bg-gray-800  w-full m-0 h-24 p-10 top-0 flex justify-between items-center">
+        <div className="AppHeader z-10 shadow-md fixed w-full m-0 h-24 p-10 top-0 flex justify-between items-center">
             <h1 className="transition-all font-bold text-3xl lg:text-4xl text-white">
                 OSK139 / M609
             </h1>
--- src/components/Find.js
+++ src/components/Find.js
@@ -3,12 +3,12 @@ import { ReactComponent as FindSvg } from "./find.svg";
 let Find = (props) => {
     const { value, onValueChange } = props;
     return (
-        <div className="find transition-all lg:w-5/12 w-2/3 shadow-inner ">
-            <label className="transition-all find-icon flex justify-center items-center focus-within:text-blue-50 text-gray-400 bg-gray-700 h-14 rounded-lg pl-3 shadow-4xl focus-within:ring-2 focus-within:ring-gray-600">
+        <div className="find transition-all lg:w-5/12 w-2/3">
+            <label className=" find-label transition-all find-icon z-10 flex justify-center items-center focus-within:text-blue-50 text-gray-400 bg-gray-700 h-14 rounded-xl pl-3 shadow-4xl">
                 <FindSvg fill="currentColor" className="icon w-6 h-6 transition-all" />
                 <input
                     type="text"
-                    className="find-input w-full transition-all outline-none bg-gray-700 text-blue-50 px-2 lg:px-4 text-2xl lg:text-3xl"
+                    className="find-input w-full transition-all outline-none bg-transparent text-blue-50 px-2 lg:px-4 text-2xl lg:text-3xl"
                     placeholder="Find Something!"
                     value={value}
                     onChange={(event) => {
