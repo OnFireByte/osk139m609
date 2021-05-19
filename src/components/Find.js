fa70bf5daf9a5be5c1ce3d08cb2ef44f6b707b1d
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2021-05-19 18:16:51 +0700

    add magnify svg icon

--- src/components/Find.js
+++ src/components/Find.js
@@ -1,16 +1,21 @@
+import "./Find.css";
+import { ReactComponent as FindSvg } from "./find.svg";
 let Find = (props) => {
     const { value, onValueChange } = props;
     return (
-        <div className="find transition-all lg:w-5/12 w-2/3 shadow-inner flex justify-center">
-            <input
-                type="text"
-                className="find-input transition-all outline-none rounded-lg shadow-4xl shadow-inner focus:ring-2 focus:ring-gray-600 transition-all bg-gray-700 text-blue-50 shadow-xl shadow-inner w-full my-10 mx-o py-2 px-2 lg:px-4 text-2xl lg:text-3xl"
-                placeholder="Find Something!"
-                value={value}
-                onChange={(event) => {
-                    onValueChange(event.target.value);
-                }}
-            />
+        <div className="find transition-all lg:w-5/12 w-2/3 shadow-inner ">
+            <label className="transition-all find-icon flex justify-center items-center focus-within:text-blue-50 text-gray-400 bg-gray-700 h-14 rounded-lg pl-3 shadow-4xl focus-within:ring-2 focus-within:ring-gray-600">
+                <FindSvg fill="currentColor" className="icon w-6 h-6 transition-all" />
+                <input
+                    type="text"
+                    className="find-input w-full transition-all outline-none bg-gray-700 text-blue-50 px-2 lg:px-4 text-2xl lg:text-3xl"
+                    placeholder="Find Something!"
+                    value={value}
+                    onChange={(event) => {
+                        onValueChange(event.target.value);
+                    }}
+                />
+            </label>
         </div>
     );
 };
