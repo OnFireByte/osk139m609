47c52972f246b0dbee861000a30b2594d24eee6d
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-05-26 21:50:05 +0700

    add

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -5,12 +5,12 @@ let AppHeader = (props) => {
     const { findValue, onFindValueChange, onLightChange } = props;
 
     return (
-        <div className="AppHeader z-10 shadow-md fixed w-full m-0 h-24 p-10 top-0 flex justify-between items-center">
+        <div className="AppHeader dark:bg-[#151f2e] bg-pink-100 opacity-[98%] z-10 shadow-md fixed w-full m-0 h-24 p-10 top-0 flex justify-between items-center">
             <span
                 onClick={() => {
                     onFindValueChange("");
                 }}
-                className="transition-all cursor-pointer header-text text-3xl md:text-4xl text-white"
+                className="transition-all cursor-pointer lg:text-5xl text-4xl break-word dark:text-white text-gray-900"
             >
                 OSK139 / M609
             </span>
